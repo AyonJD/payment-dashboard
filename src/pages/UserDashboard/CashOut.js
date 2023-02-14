8fad6d93c6845e6bc4be8acf387cef70813d2a3c
Ayon Jodder <83225232+AyonJD@users.noreply.github.com>
2023-02-14 14:35:15 +0600

    Cashout complete

--- src/pages/UserDashboard/CashOut.js
+++ src/pages/UserDashboard/CashOut.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import CashOutForm from '../../components/Forms/UserForms/CashOutForm';
 
 const CashOut = () => {
     return (
-        <div>
-            "CashOut"
+        <div className='bg-bkash h-full flex items-center'>
+            <div className="flex flex-wrap mt-4">
+                <div className="w-full mb-12 px-4">
+                    <CashOutForm title="Cash Out" />
+                </div>
+            </div>
         </div>
     );
 };
